3d14217705d8e9552fd5bf0390cd4153b2622118
mahdi22dev <idrissimahdi2020@gmail.com>
2025-05-03 20:54:58 +0100

    change fibre with gen

--- apps/api/main.go
+++ apps/api/main.go
@@ -2,16 +2,52 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	app := fiber.New()
+	router := gin.Default()
+	router.Use(CORSMiddleware())
+	router.GET("/", myGetFunction)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	if err := router.Run(":" + port); err != nil {
+		log.Panicf("error: %s", err)
+	}
+}
 
-	log.Fatal(app.Listen(":3000"))
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+type simpleMessage struct {
+	Hello   string `json:"hello"`
+	Message string `json:"message"`
+}
+
+func myGetFunction(c *gin.Context) {
+	simpleMessage := simpleMessage{
+		Hello:   "World!",
+		Message: "Subscribe to my channel!",
+	}
+
+	c.IndentedJSON(http.StatusOK, simpleMessage)
 }
