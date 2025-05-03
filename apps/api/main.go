4d8fa73030a6826a49b246ef8361c7ff1db45cc3
mahdi22dev <idrissimahdi2020@gmail.com>
2025-05-03 20:41:22 +0100

    chnage go build output path

--- apps/api/main.go
+++ apps/api/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
-}
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println(Hello("apps/api"))
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
